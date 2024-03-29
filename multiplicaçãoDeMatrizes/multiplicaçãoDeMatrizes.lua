46ad3b60d3d4835dc7bf0c66389e46d677bb5a91
ddogs <danielolibar@gmail.com>
2024-03-29 19:28:34 -0300

    Added extra comments and improved spacing

--- multiplicaçãoDeMatrizes/multiplicaçãoDeMatrizes.lua
+++ multiplicaçãoDeMatrizes/multiplicaçãoDeMatrizes.lua
@@ -1,52 +1,81 @@
+-- Splits a string by the space (" ") separator
 function splitStringByTab(str)
+    
     local fields = {}
     local pattern = string.format("([^%s]+)", " ")
     string.gsub(str, pattern, function(c) fields[#fields+1] = c end)
+
     return fields
+
 end
 
+-- Reads a matrix from a file given a path
 function readMatrixFromFile(filePath)
+    
     local file = io.open(filePath, "r")
     local matrix = {}
     local n, m
 
     if file then
+        
         -- Reads first line with number of rows n and columns m
         local line = file:read("*l")
         local dimensions = splitStringByTab(line, " ")
+
         n = tonumber(dimensions[1])
         m = tonumber(dimensions[2])
 
         -- Reads matrix values
         for i = 1, n do
+
             line = file:read("*l")
             local row = splitStringByTab(line, " ")
+
             matrix[i] = {}
+
             for j = 1, m do
+
                 matrix[i][j] = tonumber(row[j])
+
             end
+
         end
 
         file:close()
+
     else
+
         error("Could not open the file.")
+
     end
 
     return matrix, n, m
+
 end
 
+-- Prints the dimensions of a matrix and the matrix itself
 function printMatrix(matrix)
+    
     local n = #matrix
     local m = #matrix[1]
+
     print(string.format("Dimensions: %d x %d", n, m))
+
     for i = 1, n do
+
         for j = 1, m do
+
             io.write(string.format("%d ", matrix[i][j]))
+
         end
+
         print()
+
     end
+
 end
 
+-- Multiplies two matrices
 function multiplyMatrix(matrixA, matrixB)
 
     local nC = #matrixA
@@ -84,7 +113,6 @@ end
 local matrixA, nA, mA = readMatrixFromFile("./multiplicaçãoDeMatrizes/exemplos/matrizes/A.txt")
 local matrixB, nB, mB = readMatrixFromFile("./multiplicaçãoDeMatrizes/exemplos/matrizes/B.txt")
 local matrixC, nC, mC = readMatrixFromFile("./multiplicaçãoDeMatrizes/exemplos/matrizes/C.txt")
-
 
 local matrixA_B = multiplyMatrix(matrixA, matrixB)
 local matrixB_C = multiplyMatrix(matrixB, matrixC)
