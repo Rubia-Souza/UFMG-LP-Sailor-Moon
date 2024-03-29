e19723de2ae65abfba08949c7112e700b9a014b4
ddogs <danielolibar@gmail.com>
2024-03-29 19:22:45 -0300

    Created funciton to multiply two matrices. Also made changes to the print matrix function. Added new examples of matrix multiplication results.

--- multiplicaçãoDeMatrizes/multiplicaçãoDeMatrizes.lua
+++ multiplicaçãoDeMatrizes/multiplicaçãoDeMatrizes.lua
@@ -35,7 +35,9 @@ function readMatrixFromFile(filePath)
     return matrix, n, m
 end
 
-function printMatrix(matrix, n, m)
+function printMatrix(matrix)
+    local n = #matrix
+    local m = #matrix[1]
     print(string.format("Dimensions: %d x %d", n, m))
     for i = 1, n do
         for j = 1, m do
@@ -45,6 +47,67 @@ function printMatrix(matrix, n, m)
     end
 end
 
-local matrix, n, m = readMatrixFromFile("./multiplicaçãoDeMatrizes/exemplos/matrizes/A.txt")
+function multiplyMatrix(matrixA, matrixB)
 
-printMatrix(matrix, n, m)
+    local nC = #matrixA
+    local mC = #matrixB[1]
+    local innerDimension = #matrixA[1]
+
+    local matrixC = {}
+
+    for i = 1, nC do
+
+        matrixC[i] = {}
+
+        for j = 1, mC do
+
+            local mult = 0
+            local sum = 0
+
+            for k = 1, innerDimension do
+
+                mult = matrixA[i][k] * matrixB[k][j]
+                sum = sum + mult
+
+            end
+
+            matrixC[i][j] = sum
+
+        end
+
+    end
+
+    return matrixC
+
+end
+
+local matrixA, nA, mA = readMatrixFromFile("./multiplicaçãoDeMatrizes/exemplos/matrizes/A.txt")
+local matrixB, nB, mB = readMatrixFromFile("./multiplicaçãoDeMatrizes/exemplos/matrizes/B.txt")
+local matrixC, nC, mC = readMatrixFromFile("./multiplicaçãoDeMatrizes/exemplos/matrizes/C.txt")
+
+
+local matrixA_B = multiplyMatrix(matrixA, matrixB)
+local matrixB_C = multiplyMatrix(matrixB, matrixC)
+local matrixA_B_C = multiplyMatrix(matrixA, matrixB_C)
+local matrixB_C_A = multiplyMatrix(matrixB_C, matrixA)
+local matrixB_C_B = multiplyMatrix(matrixB_C, matrixB)
+
+print("--------- matrix matrixA_B ---------")
+printMatrix(matrixB_C_B)
+print()
+
+print("--------- matrix matrixB_C ---------")
+printMatrix(matrixB_C)
+print()
+
+print("--------- matrix matrixA_B_C ---------")
+printMatrix(matrixA_B_C)
+print()
+
+print("--------- matrix matrixB_C_A ---------")
+printMatrix(matrixB_C_A)
+print()
+
+print("--------- matrix matrixB_C_B ---------")
+printMatrix(matrixB_C_B)
+print()
